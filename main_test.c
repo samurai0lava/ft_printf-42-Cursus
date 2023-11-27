bf12e403a2a2629198b921993a928c4197a6b1c3
Ilyass Ouhsseine <101465486+samurai0lava@users.noreply.github.com>
2023-11-27 20:31:11 +0000

    adding from the Makefile

--- main_test.c
+++ main_test.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <limits.h>
 #include "ft_printf.h"
 
 int main (void)
 {
-	char c = 'a';
-	char s[] = "TESST 1337 smaurai0lava";
-	int max = 2147483647;
-	int min = -2147483648;
-	int nul = 0;
-	unsigned int ui = 1337;
-	unsigned int nui = -5;
-	char *p = &c;
-	int *pt = &min;
-	long max_l = LONG_MAX;
-	long min_l = LONG_MIN; 
+	char 			c = 'a';
+	char 			s[] = "TESST 1337 42 smaurai0lava";
+	int 			max = 2147483647;
+	int 			min = -2147483648;
+	int 			nul = 0;
+	unsigned int 	ui = 1337;
+	unsigned int 	nui = -5;
+	char 			*p = &c;
+	int 			*pt = &min;
+	long 			max_l = LONG_MAX;
+	long 			min_l = LONG_MIN;
+	int				nbr; 
+	int				num;
+
+	nbr = 0;
+	num = 0;
+
 	ft_printf("\n******************************************--  my printf: --*****************************************\n");
-	int nbr = ft_printf("character :  %c\n", c);
+	nbr += ft_printf("character :      %c\n", c);
 	nbr += ft_printf("string :         %s\n", s);
 	nbr += ft_printf("decimal max :    %d\n", max);
 	nbr += ft_printf("integer min :    %i\n", min);
@@ -27,7 +32,6 @@ int main (void)
 	nbr += ft_printf("pointer of min : %p\n", pt);
 	nbr += ft_printf("null in hex :    %x\n", nul);
 	nbr += ft_printf("max in hex :     %X\n", max);
-	nbr += ft_printf("NULL STR :       %s", (char *)NULL);
 	nbr += ft_printf("                 %%\n");
 	nbr += ft_printf("POINTER LONG     %p \n%p\n ", &min_l, &max_l);
 	nbr += ft_printf("                 %p\n", (void *)-14523);
@@ -36,7 +40,7 @@ int main (void)
 	nbr += ft_printf("S4%Xf`<f$J '%X@%i!EAsCK4%xy_%d)-7l3Mjln%pf1+q6%%%Xo{{ulxmt%i{dNsv4A<0%X`f}Qf\n", -1471565265, -1471859136, 1415328167, -11470702, 240168234, (void *)-1415595296739750836, -1039313513, 1875270030, -707402165);
 
 	printf("\n*****************************************-- original printf: --********************************************\n");
-	int num = printf("character :   %c\n", c);
+	num += printf("character :      %c\n", c);
 	num += printf("string :         %s\n", s);
 	num += printf("decimal max :    %d\n", max);
 	num += printf("integer min :    %i\n", min);
@@ -46,7 +50,6 @@ int main (void)
 	num += printf("pointer of min : %p\n", pt);
 	num += printf("null in hex :    %x\n", nul);
 	num += printf("max in hex :     %X\n", max);
-	num += printf("NULL STR :       %s\n", (char *)NULL);
 	num += printf("                 %%\n");
 	num += printf("POINTER LONG     %p\n%p\n", &min_l, &max_l);
 	num += printf("                 %p\n", (void *)-14523);
